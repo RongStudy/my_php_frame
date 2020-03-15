6b5f1eaa0633ab3b90a15c3cbeac056e0f8967bf
荣洪源 <rong_study1996@163.com>
2020-03-15 17:53:19 +0800

    demo

--- App/Controller/Index.php
+++ App/Controller/Index.php
@@ -2,22 +2,29 @@
 
 namespace App\Controller;
 
-class Index extends \Frame\Controller
+use Frame\App;
+use Frame\Controller;
+
+class Index extends Controller
 {
+    /**
+     * 使用 twig 模板
+     */
     public function index()
     {
-        $array = ['yes' => 'one'];
-        $this->assign('data', $array);
-        $this->display();
+        $html = '<h1 style="text-align: center;">Hello My_Frame</h1>';
+        $this->assign('data', $html);
+        $this->assign('data2', ['num1' => 1, 'num2' => ['num22' =>22]]);
+        $this->render();
     }
 
+    /**
+     * 不使用 twig 模板
+     */
     public function index2()
     {
-        echo 1231;
-    }
-
-    public function tester()
-    {
-        $this->display();
+        $html = '<h1 style="text-align: center;">Hello My_Frame</h1>';
+        $this->assign('data', $html);
+        $this->display('ss');
     }
 }
