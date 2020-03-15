54ffd7b0a1457bacbd62e32d3509ffaa8f040249
ronghongyuan <rong_study1996@163.com>
2020-03-15 17:45:11 +0800

    更新Controller，兼容 twig 模板

--- Frame/Controller.php
+++ Frame/Controller.php
@@ -9,10 +9,10 @@ namespace Frame;
  */
 abstract class Controller
 {
-    protected $data;
     protected $controller_name;
     protected $action_name;
     protected $template_dir;
+    protected $assign_data = array();
 
     public function __construct($controller_name, $action_name)
     {
@@ -21,15 +21,28 @@ abstract class Controller
         $this->template_dir = Application::getInstance()->base_dir . '/App/View';
     }
 
-    public function assign($key, $value)
+    public function assign($key = '', $value = null)
     {
-        $this->data[$key] = $value;
+        if (is_array($value)) {
+            foreach ($value as $key => $val) {
+                $this->assign_data[$key] = $val;
+            }
+        } else {
+            $this->assign_data[$key] = $value;
+        }
     }
 
+    /**
+     * 无模板引擎
+     * @param string $file
+     * @throws \Exception
+     */
     public function display($file = '')
     {
+        $suffix = App::getInstance()->getConfig('config');
+        $suffix = $suffix['view_suffix'] ?? 'php';
         if (empty($file)) {
-            $file = strtolower($this->controller_name) . '/' . $this->action_name . '.php';
+            $file = strtolower($this->controller_name) . '/' . strtolower($this->action_name) . ".{$suffix}";
         } else {
             $file = str_replace('\\', '/', $file);
             $file = trim($file);
@@ -37,24 +50,61 @@ abstract class Controller
             $hasx = strpos($file, '/');
             if ($hasx !== false) {
                 $file_arr = explode('/', $file);
-                if (!$file_arr[0] == $this->controller_name) {
-                    $file = $this->controller_name . '/' . $file;
-                }
+                $file = strtolower($file_arr[0]) . '/' . strtolower($file_arr[1]);
             } else {
-                $file = $this->controller_name . '/' . $file;
+                $file = strtolower($this->controller_name) . '/' . $file;
             }
-
-            if (substr($file, -4, strlen($file)) != '.php') {
-                $file .= '.php';
+            if (substr($file, -4, strlen($file)) != ".{$suffix}") {
+                $file .= ".{$suffix}";
             }
         }
         $path = $this->template_dir . '/' . $file;
         if (!file_exists($path)) {
-            exit($path . ' is not exists');
+            throw_except($path . ' is not exists!');
         }
-        if (!empty($this->data)) {
-            extract($this->data);
+        if (!empty($this->assign_data)) {
+            extract($this->assign_data);
         }
         require_once $path;
+    }
+
+    /**
+     * twig 模板引擎
+     * @param string $path
+     * @throws \Exception
+     */
+    public function render($path = '')
+    {
+        $suffix = App::getInstance()->getConfig('config');
+        $suffix = $suffix['view_twig_suffix'] ?? 'twig';
+        if (empty($path)) {
+            $path = strtolower($this->controller_name) . '/' . strtolower($this->action_name);
+        } else {
+            $path = str_replace('\\', '/', $path);
+            $path = trim($path);
+            $path = trim($path, '/');
+            $hasx = strpos($path, '/');
+            if ($hasx !== false) {
+                $path_arr = explode('/', $path);
+                $path = strtolower($path_arr[0]) . '/' . $path_arr[1];
+            } else {
+                $path = strtolower($this->controller_name) . '/' . $path;
+            }
+        }
+        $path .= ".{$suffix}";
+        if (!file_exists($this->template_dir . '/' . $path)) {
+            throw_except(($this->template_dir . '/' . $path) . ' is not exists!');
+        }
+
+        $key = 'app_twig';
+        if (!Register::get($key)) {
+            $loader = new \Twig\Loader\FilesystemLoader($this->template_dir);
+            $twig = new \Twig\Environment($loader, [
+                'cache' => BASEDIR . '/runtime',
+                'auto_reload' => true
+            ]);
+            Register::set($key, $twig);
+        }
+        echo Register::get($key)->render($path, $this->assign_data);
     }
 }
