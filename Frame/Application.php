f9c4b42ebb04853a39d9f64edbed08a594c60eb9
ronghongyuan <rong_study1996@163.com>
2020-03-15 17:49:34 +0800

    优化Application.php

--- Frame/Application.php
+++ Frame/Application.php
@@ -7,14 +7,37 @@ namespace Frame;
 class Application
 {
     public $base_dir;
-    protected static $instance;
+    private $displtch = true;
 
-    public $config;
+    private static $config;
+    private static $instance;
 
-    protected function __construct($base_dir)
+    private function __construct($base_dir)
     {
         $this->base_dir = $base_dir;
-        $this->config = new Config($base_dir . '/configs');
+        self::$config = new Config($base_dir . '/configs');
+        $this->classAlias();
+        $this->loader();
+        $this->loger();
+    }
+
+    private function __clone(){}
+
+    private function classAlias()
+    {
+        class_alias('Frame\\Application', 'Frame\\App');
+    }
+
+    private function loader()
+    {
+        is_file($this->base_dir . '/App/common.php') ? include_once $this->base_dir . '/App/common.php' : '';
+    }
+
+    private function loger()
+    {
+        $whoops = new \Whoops\Run;
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+        $whoops->register();
     }
 
     /**
@@ -32,15 +55,19 @@ class Application
 
     public function dispatch()
     {
+        if (!$this->displtch) {
+            exit('can\'t');
+        }
+        $this->displtch = false;
         $uri = trim($_SERVER['REQUEST_URI'], '/');
         $uri_arr = explode('/', $uri);
         if ($uri == '' || $uri[0] == '?' || $uri == '/' || count($uri_arr) == 1) {
-            $controller = ucwords(trim($this->config['config']['default_controller']));
-            $default_action = $this->config['config']['default_action'];
+            $controller = ucwords(trim(self::$config['config']['default_controller']));
+            $default_action = self::$config['config']['default_action'];
             $action = strtolower($default_action[0]) . substr($default_action, 1, strlen($default_action));
         } else {
             $controller = $uri_arr[0];
-            if (strpos($uri_arr[1], '?')!==false) {
+            if (strpos($uri_arr[1], '?') !== false) {
                 $action = substr($uri_arr[1], 0, strpos($uri_arr[1], '?'));
             } else {
                 $action = $uri_arr[1];
@@ -49,12 +76,12 @@ class Application
 
         $controller_low = strtolower($controller);
         $controller = ucwords($controller);
-        $class = '\\App\\Controller\\'.$controller;
+        $class = '\\App\\Controller\\' . $controller;
         if (!method_exists($class, $action)) {
             exit($class . '::' . $action . ' is not exists');
         }
         $obj = new $class($controller, $action);
-        $controller_config = $this->config['controller'];
+        $controller_config = self::$config['controller'];
         $decorators = array(); # 装饰器
         if (isset($controller_config[$controller_low]['decorator'])) {
             $conf_decorator = $controller_config[$controller_low]['decorator'];
@@ -73,5 +100,16 @@ class Application
         }
     }
 
-
+    /**
+     * 获取配置
+     * @param string $file_name 配置文件名
+     * @return array|mixed
+     */
+    public function getConfig($file_name = '')
+    {
+        if ($file_name) {
+            return self::$config[$file_name];
+        }
+        return array();
+    }
 }
