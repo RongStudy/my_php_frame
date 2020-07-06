6e19ef1359c461b13f12f46716530dbe8d85b398
ronghongyuan <ronghongyuan@doushen.com>
2020-07-06 14:09:55 +0800

    优化默认的Index控制器

--- App/Controller/Index.php
+++ App/Controller/Index.php
@@ -2,24 +2,25 @@
 
 namespace App\Controller;
 
-use Frame\App;
+use Exception;
 use Frame\Controller;
 
 class Index extends Controller
 {
     /**
      * 使用 twig 模板
+     * @throws Exception
      */
     public function index()
     {
-        $html = '<h1 style="text-align: center;">Hello My_Frame</h1>';
+        $html = '<h1 style="text-align: center;">Hello My_Frame😉</h1>';
         $this->assign('data', $html);
-        $this->assign('data2', ['num1' => 1, 'num2' => ['num22' =>22]]);
         $this->render();
     }
 
     /**
      * 不使用 twig 模板
+     * @throws Exception
      */
     public function index2()
     {
